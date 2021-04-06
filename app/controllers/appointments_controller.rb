7266369da87b3b045bc1099380a5b8aff812122d
ernesto <ernesto002@icloud.com>
2021-04-06 15:35:51 -0700

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,4 +3,22 @@ class AppointmentsController < ApplicationController
     def show
         @appointment = Appointment.find(params[:id])
     end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def index
+    end
+
+    def delete
+    end
 end
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -7,4 +7,19 @@ class PatientsController < ApplicationController
     def show 
         @patient = Patient.find(params[:id])
     end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def delete
+    end
 end
