ba0a82e346c4a31e207bd5091b591fd45ecbab86
ernesto <ernesto002@icloud.com>
2021-04-06 15:35:03 -0700

    commit tree a6a25fc18c11107d5eb0606788310e85ea803631

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -3,4 +3,22 @@ class DoctorsController < ApplicationController
     def show 
         @doctor = Doctor.find(params[:id])
     end
+    
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def index
+    end
+
+    def delete
+    end
 end
